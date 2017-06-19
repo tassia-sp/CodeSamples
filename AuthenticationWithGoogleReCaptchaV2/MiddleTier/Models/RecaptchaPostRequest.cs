b4a85da1569e65978d3e3c9e6efc1c653ae61290
Tassia <tassiapaschoal@gmail.com>
2017-06-19 14:04:30 -0700

    Added request model

---
+++ AuthenticationWithGoogleReCaptchaV2/MiddleTier/Models/RecaptchaPostRequest.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Configuration;
+
+namespace Aic.Web.Models.Requests
+{
+    public class RecaptchaPostRequest
+    {
+        [Required]
+        public string secret {
+            get
+            {
+                return ConfigurationManager.AppSettings["RecaptchaSecretKey"];
+            }
+        }
+        [Required]
+        public string response { get; set; }
+    }
+}
